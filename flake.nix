ddbf3661291cdaca65ef094620679c01ca8e33d5
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:49:38 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     wordwarvi_src.url = "https://master.dl.sourceforge.net/project/wordwarvi/wordwarvi/wordwarvi-1.00/wordwarvi-1.00.tar.gz";
@@ -35,7 +35,7 @@
             '';
 
             nativeBuildInputs = with pkgs; [
-              pkgconfig
+              pkg-config
             ];
 
             buildInputs = with pkgs; [
