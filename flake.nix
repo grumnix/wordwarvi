7581bd2524f0c23b5b6ba67f6198793b2be4fd4e
Ingo Ruhnke <grumbel@gmail.com>
2025-06-11 21:40:04 +0200

    Update to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     wordwarvi_src.url = "https://master.dl.sourceforge.net/project/wordwarvi/wordwarvi/wordwarvi-1.00/wordwarvi-1.00.tar.gz";
